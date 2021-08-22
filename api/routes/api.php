ac75dd064273d4493e6594cdb4a12b7b0d7c3ca7
YutoHayashi <y_hayashi@alta.co.jp>
2021-08-22 22:39:27 +0900

    言語対応

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group( '/v1/{locale}', function( $locale ) {
+    if ( !in_array( $locale, [ 'ja', 'en' ] ) ) abort( 404 );
+    App::setLocale( $locale );
+
+    //
+
+} );
